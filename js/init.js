f9fb49c8e573c0ee7d745062521e762b01ffc979
hiroyuki okada <ok.oakda.hiroyuki@gmail.com>
2015-05-24 22:19:32 +0900

    add pages

--- js/init.js
+++ js/init.js
@@ -1,12 +1,36 @@
-(function($){
-  $(function(){
-
-    // Plugin initialization
-    $('.button-collapse').sideNav({'edge': 'left'});
-    $('.parallax').parallax();
-    $('.collapsible').collapsible({
-      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-    });
-
-  }); // end of document ready
+(function($) {
+    $(function() {
+        writeFooter();
+        // Plugin initialization
+        $('.button-collapse').sideNav({
+            'edge': 'left'
+        });
+        $('.parallax').parallax();
+        $('.collapsible').collapsible({
+            accordion: true // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+        });
+        $('.scrollspy').scrollSpy();
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function writeSideNav() {
+    $.ajax({
+        url: "sidenav.html", //パスはcommon.jsが読み込まれたHTMLファイルが基準になります
+        cache: false, //キャッシュを利用するか（お好みで）
+        async: false, //非同期で読み込むか（お好みで）
+        success: function(html) {
+            $(".side-nav").html(html);
+        }
+    });
+}
+
+function writeFooter() {
+    $.ajax({
+        url: "footer.html", //パスはcommon.jsが読み込まれたHTMLファイルが基準になります
+        cache: false, //キャッシュを利用するか（お好みで）
+        async: false, //非同期で読み込むか（お好みで）
+        success: function(html) {
+            $(".page-footer").html(html);
+        }
+    });
+}
